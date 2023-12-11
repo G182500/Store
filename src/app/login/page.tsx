6de08e54734a50d0a20744e00d6d33f751773292
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2023-12-11 11:25:30 -0300

    feat: react-hook-form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,41 @@
 import GenericCard from "@/components/Cards/GenericCard";
+//import { useForm, SubmitHandler } from "react-hook-form";
+
+type IFormInput = {
+  user: string,
+  password: string
+}
 
 export default async function Login() {
+  /*const { handleSubmit, register } = useForm<IFormInput>();
+
+  const submit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data.user);
+  }*/
+
+  /*
+  <form onSubmit={handleSubmit(submit)}>
+    <input {...register("user")} />
+    <input {...register("password")} />
+    <input type="submit" value="Submeter"/>
+  </form> 
+  */
+
   return (
     <GenericCard title="Bem-vindo">
-      <div className="flex flex-row gap-2">
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col gap-2">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-0">
+        <form className="flex flex-col gap-4">
+          <div className="grid grid-rows-2">
             <label>Usuário</label>
-            <input className="text-black text-xs rounded-lg bg-white p-2 w-60" />
+            <input className="text-black text-xs rounded-lg bg-white p-2 w-56" />
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="grid grid-rows-2">
             <label>Senha</label>
-            <input className="text-black text-xs rounded-lg bg-white p-2 w-60" />
-            <label className="text-sm">Esqueci minha senha</label>
+            <input className="text-black text-xs rounded-lg bg-white p-2 w-56" />
           </div>
-        </div>
-        
+          <input type="submit" value="Submeter" className="bg-cyan-600 p-1 w-56"/>
+        </form>
+        <p>TESTE</p>
       </div>
     </GenericCard>
   );
